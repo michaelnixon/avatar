03de3ce11f38abd70884dc67a7bd8bc526de4244
bernuly <bernuly@gmail.com>
2014-06-03 18:04:59 -0700

    adding binaries

---
+++ SmartBodyOgreViewer/setupBinary.bat
@@ -0,0 +1,21 @@
+REM copying the config files
+cp configs\* bin\Debug
+
+REM copying the necessary libraries
+cp C:\src\smartbody\core\smartbody\SmartBody\lib\SmartBody_d.dll bin\Debug
+cp C:\src\smartbody\lib\activemq\activemq-cpp\vs2010-build\DebugDLL\activemq-cppd.dll bin\Debug
+cp C:\src\smartbody\lib\pthreads\lib\pthreadVSE2.dll bin\Debug
+cp C:\src\smartbody\lib\vhcl\openal\libs\Win32\alut.dll bin\Debug
+cp "C:\src\OgreSDK_vc10_v1-8-1\bin\debug\OgreMain_d.dll" bin\Debug
+cp "C:\src\OgreSDK_vc10_v1-8-1\bin\debug\OIS_d.dll" bin\Debug
+cp "C:\src\smartbody\core\smartbody\SmartBody\src\external\glew\glew32.dll" bin\Debug
+cp "C:\src\smartbody\lib\xerces-c\bin\xerces-c_3_1D.dll" bin\Debug
+cp C:\src\smartbody\lib\boost\lib\boost_system-vc100-mt-gd-1_51.dll bin\Debug
+cp C:\src\smartbody\lib\boost\lib\boost_thread-vc100-mt-gd-1_51.dll bin\Debug
+cp C:\src\smartbody\lib\boost\lib\boost_date_time-vc100-mt-gd-1_51.dll bin\Debug
+cp "C:\src\smartbody\lib\boost\lib\boost_filesystem-vc100-mt-gd-1_51.dll" bin\Debug
+cp "C:\src\smartbody\lib\boost\lib\boost_python-vc100-mt-gd-1_51.dll" bin\Debug
+cp "C:\src\smartbody\lib\vhcl\libsndfile\bin\libsndfile-1.dll" bin\Debug
+cp "C:\src\smartbody\core\smartbody\steersuite-1.3\build\win32\Debug\steerlibd.dll" bin\Debug
+cp "C:\src\OgreSDK_vc10_v1-8-1\bin\debug\RenderSystem_GL_d.dll" bin\Debug
+cp "C:\src\OgreSDK_vc10_v1-8-1\bin\debug\RenderSystem_Direct3D9_d.dll" bin\Debug
